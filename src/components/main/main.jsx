e49841b8711bed781dfecf7ca1f3503957a1bf27
Abubakar-Chonnoo_crdn <abubakar.chonnoo@dayforce.com>
2025-07-13 16:12:29 +0400

    Adding Styling For Main Component

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -5,7 +5,11 @@ import Lists from "@/components/lists/lists";
 
 const Main = () => {
   return (
-    <div className="main-container">
+    <div className={styles["main-container"]}>
+      <div className={styles.header}>
+        <h1>Today's Tasks</h1>
+        <p>1 of 5 tasks completed</p>
+      </div>
       <Task />
       <Filter />
       <Lists />
